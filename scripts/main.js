d6e53578eb097195ae1596c08c5f96166e7be237
martsul <sasham9321@mail.ru>
2024-09-20 20:51:05 +0300

    before clock

--- scripts/main.js
+++ scripts/main.js
@@ -10,11 +10,11 @@ const teamSwiper = new Swiper(".team__swiper", {
   breakpoints: {
     966: {
       slidesPerView: 3,
-    }, 
+    },
     520: {
       slidesPerView: 2,
-    }
-  }
+    },
+  },
 });
 
 const reviewSwiper = new Swiper(".review__swiper", {
@@ -61,11 +61,13 @@ document.querySelectorAll(".menu__close, .menu__arrow").forEach((e) => {
   });
 });
 
-document.querySelectorAll(".menu__mini-block").forEach(e => {
+document.querySelectorAll(".menu__mini-block").forEach((e) => {
   e.querySelector(".menu__mini-block-full").addEventListener("click", () => {
-    e.querySelector(".menu__mini-links").classList.toggle("menu__mini-links-full");
-  })
-})
+    e.querySelector(".menu__mini-links").classList.toggle(
+      "menu__mini-links-full"
+    );
+  });
+});
 
 // Aside
 
@@ -84,6 +86,20 @@ callBtn.forEach((e) => {
   e.addEventListener("click", () => {
     callPop.classList.add("pop-on-full");
     document.body.classList.add("body-hidden");
+
+    document.addEventListener("keyup", (event) => {
+      if (event.key === "Escape") {
+        callPop.classList.remove("pop-on-full");
+        document.body.classList.remove("body-hidden");
+      }
+    });
+
+    callPop.addEventListener("click", event => {
+      if (event.target === callPop) {
+        callPop.classList.remove("pop-on-full");
+        document.body.classList.remove("body-hidden");
+      }
+    })
   });
 });
 
@@ -91,3 +107,50 @@ callClose.addEventListener("click", () => {
   callPop.classList.remove("pop-on-full");
   document.body.classList.remove("body-hidden");
 });
+
+// Qwiz catalog
+
+let nextBtnCat = document.querySelector(".cost__btn-next");
+let prevBtnCat = document.querySelector(".cost__btn-prev");
+
+if (nextBtnCat) {
+  let cards = document.querySelectorAll(".cost__card");
+  let load = document.querySelector(".cost__load");
+  let line = document.querySelector(".cost__load-line");
+  let i = 0;
+
+  nextBtnCat.addEventListener("click", () => {
+    cards[i].classList.remove("cost__card-on");
+    cards[i].setAttribute("style", "display: none;");
+
+    i++;
+
+    cards[i].setAttribute("style", "display: inline-block;");
+    cards[i].classList.add("cost__card-on");
+
+    line.setAttribute(
+      "style",
+      `width: calc(${i + 1} * 100% / ${cards.length})`
+    );
+
+    prevBtnCat.setAttribute("style", "display: inline-block;");
+
+    if (i === cards.length - 1) {
+      load.setAttribute("style", "display: none;");
+    }
+  });
+
+  prevBtnCat.addEventListener("click", () => {
+    cards[i].classList.remove("cost__card-on");
+    cards[i].setAttribute("style", "display: none;");
+
+    i--;
+
+    cards[i].setAttribute("style", "display: inline-block;");
+    cards[i].classList.add("cost__card-on");
+
+    if (i === 0) {
+      prevBtnCat.setAttribute("style", "display: none;");
+    }
+  });
+}
